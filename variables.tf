bfa1dc45c78425c2a6e9a1748f06f3f52d037fb4
Mohan Sai <80935489+mohansai123@users.noreply.github.com>
2023-12-06 12:30:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "availability_zone" {
 
 variable "iam_instance_profile" {
   description = "EC2 instance IAM instance profile name"
-  default     = "dev-group"  # Update with a valid instance profile name
+  default     = "s3-user"  # Update with a valid instance profile name
 }
 
 variable "key_name" {
